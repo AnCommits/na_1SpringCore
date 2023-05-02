eebb35c11d9ce88ed1e83f2250db5f0c6a5963b3
AnUser99 <kisamasyanya@gmail.com>
2023-05-02 08:02:23 +0300

    na_1springCore_8

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -4,29 +4,38 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
+
+        System.out.println("Point 0");
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
 
-//        Music music1 = context.getBean("classicalMusicBean", Music.class);
+        System.out.println("Point 1");
+
+        Music music1 = context.getBean("classicalMusicBean", Music.class);
 //        Music music2 = context.getBean("rockMusicBean", Music.class);
 //        Music music3 = context.getBean("retroMusicBean", Music.class);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
+        System.out.println(music1.getSong());
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
 //        musicPlayer.playMusic();
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
 
         context.close();
+
+        System.out.println("Point 2");
     }
 }
